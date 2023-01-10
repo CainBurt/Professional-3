0304ae2da715fd40d96da5cab8c57a6ad88c263d
Cain Burt <cain@thisiscrowd.com>
2023-01-10 16:22:37 +0000

    slider js and css fixes

--- src/js/blocks/team.js
+++ src/js/blocks/team.js
@@ -1,16 +1,22 @@
 import Glide from '@glidejs/glide'
 
-var glide = new Glide('.glide', {
-    type: 'carousel',
-    perView: 6,
-    focusAt: 'center',
-    breakpoints: {
-      800: {
-        perView: 2
-      },
-      480: {
-        perView: 1
-      }
-    }
-})
-glide.mount()
+var sliders = document.querySelectorAll('.glide');
+for (var i = 0; i < sliders.length; i++) {
+    var glide = new Glide(sliders[i], {
+        type: 'carousel',
+        perView: 6,
+        focusAt: 'center',
+        breakpoints: {
+        800: {
+            perView: 2
+        },
+        480: {
+            perView: 1
+        }
+        }
+    })
+    glide.mount()
+}
+
+
+
