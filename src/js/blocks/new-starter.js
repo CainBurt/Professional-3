0204f246934311c6eac06c5b853f068753f9d4fb
Cain Burt <cain@thisiscrowd.com>
2023-01-13 12:44:16 +0000

    new starter slider fixes

--- src/js/blocks/new-starter.js
+++ src/js/blocks/new-starter.js
@@ -4,9 +4,14 @@ var slider = document.getElementById('start-slider');
 // for (var i = 0; i < sliders.length; i++) {
     var glide = new Glide(slider, {
         type: 'carousel',
-        perView: 8,
+        perView: 9,
         focusAt: 'center',
         autoplay: 2000,
+        breakpoints:{
+            1700:{
+                perView: 6,
+            },
+        }
     })
     glide.mount()
 
