fa187d13de57179c3d4a11484475dfacd6f1efe2
Cain Burt <cain@thisiscrowd.com>
2023-01-16 11:22:17 +0000

    added new starter form buttons and styling

--- src/js/blocks/new-starter.js
+++ src/js/blocks/new-starter.js
@@ -22,8 +22,25 @@ if(formSlider){
     var sliderForm = new Glide(formSlider, {
         type: 'slider',
         focusAt: 'center',
+        dragDistance: false,
+        swipeThreshold: false,
     })
     sliderForm.mount()
+
+    var next_btns = document.querySelectorAll('#next_btn')
+    for (var i = 0; i < next_btns.length; i++){
+        next_btns[i].addEventListener('click', function(){
+            sliderForm.go('>')
+        })
+    }
+
+    var back_btns = document.querySelectorAll('#back_btn')
+    for (var j = 0; j < back_btns.length; j++){
+        back_btns[j].addEventListener('click', function(){
+            sliderForm.go('<')
+        })
+    }
+    
 }
 
 
