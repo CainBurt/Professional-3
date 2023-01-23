270f3cadaee4bb5e7510c1dcb590799bc3208ba9
Cain Burt <cain@thisiscrowd.com>
2023-01-23 11:04:45 +0000

    validation and styling done for name and address slides

--- src/js/blocks/new-starter.js
+++ src/js/blocks/new-starter.js
@@ -28,55 +28,56 @@ if(formSlider){
         dragDistance: false,
         swipeThreshold: false,
     })
-    sliderForm.mount()
+    sliderForm.mount();
 
-    var next_btns = document.querySelectorAll('#next_btn')
+    var next_btns = document.querySelectorAll('#next_btn');
     for (var i = 0; i < next_btns.length; i++){
         next_btns[i].addEventListener('click', function(){
-            sliderForm.go('>')
-        })
-    }
+            sliderForm.go('>');
+        });
+    };
 
-    var back_btns = document.querySelectorAll('#back_btn')
+    var back_btns = document.querySelectorAll('#back_btn');
     for (var j = 0; j < back_btns.length; j++){
         back_btns[j].addEventListener('click', function(){
-            sliderForm.go('<')
-        })
-    }
+            sliderForm.go('<');
+        });
+    };
     
-    var input_fields = document.querySelectorAll(".wpcf7-text")
-    var name = document.getElementById("name")
-    var address1 = document.getElementById("address1")
-    var city = document.getElementById("city")
-    var postcode = document.getElementById("postcode")
-    var country = document.getElementById("country")
-    var detailsStatus = document.getElementById("details_status")
+    var input_fields = document.querySelectorAll(".wpcf7-text");
+    var name = document.getElementById("name");
+    var address1 = document.getElementById("address1");
+    var city = document.getElementById("city");
+    var postcode = document.getElementById("postcode");
+    var country = document.getElementById("country");
+    var detailsStatus = document.getElementById("details_status");
 
-    var file_fields = document.querySelectorAll(".wpcf7-file")
-    var doc1 = document.getElementById("file1")
-    var doc2 = document.getElementById("file2")
-    var doc3 = document.getElementById("file3")
-    var filesStatus = document.getElementById("files_status")
+    var file_fields = document.querySelectorAll(".wpcf7-file");
+    var doc1 = document.getElementById("file1");
+    var doc2 = document.getElementById("file2");
+    var doc3 = document.getElementById("file3");
+    var filesStatus = document.getElementById("files_status");
 
 
 
     input_fields.forEach(input => {
         input.addEventListener('change', function(){
-            if(name.value == '' || address1.value == '' || city == '' || postcode == '' || country == ''){
-                // console.log("some fields are empty")
+            
+            if(name.value == '' || address1.value == '' || city.value == '' || postcode.value == '' || country.value == ''){
                 detailsStatus.style.color = 'red';
-                document.getElementById('sub-status').innerHTML = 'Missing'
+                document.getElementById('sub-status').innerHTML = 'Missing'             
             }else{
                 detailsStatus.style.color = 'Green';
                 document.getElementById('sub-status').innerHTML = 'Completed'
             }
+
         })
-    })
+    });
 
     var details_link = document.getElementById('details_link')
     details_link.addEventListener('click', function(){
         sliderForm.go('=1')
-    })
+    });
 
     file_fields.forEach(file => {
         file.addEventListener('input', function(){
@@ -91,12 +92,57 @@ if(formSlider){
             }
             
         })
-    })
+    });
 
     var files_link = document.getElementById('files_link')
     files_link.addEventListener('click', function(){
         sliderForm.go('=4')
-    })
+    });
+
+    // Event listeners for buttons
+    name.addEventListener('change', function(){
+        var childBtn = name.parentElement.parentElement.querySelector('.next-btn')
+        if(name.value == '' || name.value == null){
+            childBtn.classList.add("disabled");
+        }else if (name.value != '') {
+            childBtn.classList.remove("disabled");
+        }
+    });
+    
+    [address1, city, postcode, country].forEach(function(element){
+        element.addEventListener('change', function(){
+        
+            var childBtn = address1.parentElement.parentElement.querySelector('.next-btn');
+            console.log(childBtn)
+            if(address1.value == '' || city.value == '' || postcode.value == '' || country.value == ''){
+                console.log("empty")
+                childBtn.classList.add("disabled");
+            }else{
+                childBtn.classList.remove("disabled");
+            }
+    
+        });
+    });
+
+    name.addEventListener('change', function(){
+        var childBtn = name.parentElement.parentElement.querySelector('.next-btn')
+        if(name.value == '' || name.value == null){
+            childBtn.classList.add("disabled");
+        }else if (name.value != '') {
+            childBtn.classList.remove("disabled");
+        }
+    });
+    
+    
+    // function allAddressFilled(){
+    //     if(address1.value == '' || city.value == '' || postcode.value == '' || country.value == ''){
+    //         return false
+    //     }else if (address1.value != '' && city.value != '' && postcode.value != '' && country.value != ''){
+    //         return true
+    //     }
+    // }
+
+
 }
     
 
