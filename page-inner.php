a10ca0d21fb67781c65296f0a056b8010303d1fe
Cain Burt <cain@thisiscrowd.com>
2023-03-03 11:31:26 +0000

    added sub page template

---
+++ page-inner.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Sub Page
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+$context['navigation'] = new TimberMenu($post->get_field('sidebarnavigation'));
+preg_match_all('/<(h\d*).*?(id="(.*?)").*?>(.*?)<\//',$post->content,$matches);
+$levels = $matches[1];
+$anchors = $matches[3];
+$headings = $matches[4];
+$context['anchors'] = array();
+foreach ($anchors as $key => $anchor) {
+    $context['anchors'][] = array(
+        'anchor' => $anchor,
+        'heading' => $headings[$key]
+    );
+}
+Timber::render( array( 'page-inner.twig', 'page.twig' ), $context );
