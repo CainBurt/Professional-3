623e047cbd34b0c1c1e4f16507d12451c1349474
Cain Burt <cain@thisiscrowd.com>
2023-03-03 10:25:37 +0000

    change team grid layout

--- src/js/blocks/team.js
+++ src/js/blocks/team.js
@@ -1,41 +1,41 @@
-import Glide from '@glidejs/glide'
+// import Glide from '@glidejs/glide'
 
-var rows = document.querySelectorAll('.team-row')
-var sliders = document.querySelectorAll('#team-sliders');
-if(sliders){
-    for (var i = 0; i < sliders.length; i++) {
-        if(rows[i].style.display !== 'none'){
-            var glide = new Glide(sliders[i], {
-                type: 'slider',
-                perView: 8,
-                // focusAt: 'center',
-                autoplay: 30000 + Math.floor(Math.random() * 5000),
-                rewind: false,
-                breakpoints: {
-                    2000:{
-                        perView: 7,
-                    },
-                    1700:{
-                        perView: 5,
-                    },
-                    1400:{
-                        perView: 5,
-                    },
-                    1100:{
-                        perView: 3,
-                    },
-                    800: {
-                        perView: 3
-                    },
-                    480: {
-                        perView: 1
-                    }
-                }
-            })
-            glide.mount()
-        }
+// var rows = document.querySelectorAll('.team-row')
+// var sliders = document.querySelectorAll('#team-sliders');
+// if(sliders){
+//     for (var i = 0; i < sliders.length; i++) {
+//         if(rows[i].style.display !== 'none'){
+//             var glide = new Glide(sliders[i], {
+//                 type: 'slider',
+//                 perView: 8,
+//                 // focusAt: 'center',
+//                 autoplay: 30000 + Math.floor(Math.random() * 5000),
+//                 rewind: false,
+//                 breakpoints: {
+//                     2000:{
+//                         perView: 7,
+//                     },
+//                     1700:{
+//                         perView: 5,
+//                     },
+//                     1400:{
+//                         perView: 5,
+//                     },
+//                     1100:{
+//                         perView: 3,
+//                     },
+//                     800: {
+//                         perView: 3
+//                     },
+//                     480: {
+//                         perView: 1
+//                     }
+//                 }
+//             })
+//             glide.mount()
+//         }
         
-    }
-}
+//     }
+// }
 
 
