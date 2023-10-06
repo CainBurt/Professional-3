5bc1e4b0d59c737fcabcc9c300a7125bdc6c97b9
Cain Burt <cain@thisiscrowd.com>
2023-10-06 08:52:09 +0100

    fix download list name in tracking page

--- includes/blocks/download-list.php
+++ includes/blocks/download-list.php
@@ -22,6 +22,8 @@ function download_list_render_callback( $block, $content = '', $is_preview = tru
     // Store $is_preview value.
     $context['is_preview'] = $is_preview;
 
+    $current_page = Timber::get_post();
+    $context['current_page_title'] = $current_page->title;
     // Render the block.
     Timber::render( 'components/blocks/download-list.twig', $context);
 }
--- includes/tracking/tracking.php
+++ includes/tracking/tracking.php
@@ -1,10 +1,53 @@
 <?php
+// Function to export tracking data to CSV
+function export_tracking_data_to_csv() {
+    // Define the CSV filename and headers
+
+    $filename = 'tracking_data.csv';
+
+    // Create a file pointer and write CSV data
+    $file = fopen('php://output', 'w');
+    header('Content-Type: text/csv');
+    header('Content-Disposition: attachment; filename="' . $filename . '"');
+
+    // Add the header row
+    fputcsv($file, array('User', 'Read Employee Handbook', 'Read Security Policy', /* Add more columns here */));
+
+    // Retrieve actual tracking data from your database
+    $users = get_users(array('role' => 'subscriber')); // Adjust the query as needed
+
+    foreach ($users as $user) {
+        $user_data = array(
+            $user->display_name,
+            get_user_meta($user->ID, 'read_handbook', true),
+            get_user_meta($user->ID, 'read_security', true),
+            // Add more data retrieval here for additional columns
+        );
+
+        // Add the user data to the CSV
+        fputcsv($file, $user_data);
+    }
+
+    fclose($file);
+    exit;
+}
+
+
+// Hook to add the CSV export action
+add_action('admin_init', 'register_csv_export_action');
+function register_csv_export_action() {
+    if (isset($_POST['export_csv'])) {
+        export_tracking_data_to_csv();
+    }
+}
+
+
 // Function to display the tracking information
 function display_tracking_page() {
     ?>
     <div class="wrap">
         <h2>Tracking Page</h2>
-        <!-- <button id="export-button" class="button">Export</button> -->
+        <a class="button" href="<?php echo admin_url('admin.php?page=export-csv'); ?>">Export to CSV</a>
         <table class="tracking-table fixed striped">
             <thead>
                 <tr>
@@ -23,11 +66,18 @@ function display_tracking_page() {
                         foreach ($user_meta as $meta_key => $meta_value) {
                             if (strpos($meta_key, 'clicked_') === 0) {
                                 $column_name = str_replace('_', ' ', ucwords(str_replace('clicked_', 'Clicked ', $meta_key))); // Format the column name
+                                $page = '';
+                                // Check if the column name contains '(SOPs)' and modify it accordingly
+                                if (strpos($column_name, '(SOPs)') !== false) {
+                                    $page .= '<span style="color: var(--orange)">(SOP)</span>';
+                                    $column_name = str_replace('Clicked ', '', $column_name);
+                                    $column_name = str_replace('(SOPs)', '', $column_name);
+                                }
 
                                 if (!in_array($column_name, $added_columns)) {
                                     $column_keys[$meta_key] = true;
                                     $added_columns[] = $column_name;
-                                    echo '<th>' . esc_html($column_name) . '</th>';
+                                    echo '<th>' . $page . esc_html($column_name) . '</th>';
                                 }
                             }
                         }
@@ -68,6 +118,8 @@ function display_tracking_page() {
 // Function to add the menu page
 function add_tracking_menu_page() {
     add_menu_page('Tracking Page', 'Tracking Page', 'manage_options', 'tracking-page', 'display_tracking_page');
+    add_submenu_page('tracking-page', 'Export to CSV', 'Export to CSV', 'manage_options', 'export-csv', 'export_tracking_data_to_csv');
+
 }
 
 // Hook to add the menu page
